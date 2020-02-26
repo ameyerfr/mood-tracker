33e7b5bbaa9cd517addf05694258a280b225e072
Yannick Bourenane <yannick.bourenane@gmail.com>
2020-02-26 16:44:41 +0100

    missing coma model

--- server/models/DayMood.model.js
+++ server/models/DayMood.model.js
@@ -1,16 +1,16 @@
 const mongoose = require("mongoose");
-var DateOnly = require('mongoose-dateonly')(mongoose);
+var DateOnly = require("mongoose-dateonly")(mongoose);
 const Schema = mongoose.Schema;
 
 const schema = new Schema({
   owner: {
-      type: Schema.Types.ObjectId,
-      ref: "User"
-    },
-  day: { type : DateOnly, required : true, default : Date.now()},
-  mood : { type : Number, required : true, min : 0, max : 10 }
-  k_good: { type : [String] },
-  k_bad: { type : [String] }
+    type: Schema.Types.ObjectId,
+    ref: "User"
+  },
+  day: { type: DateOnly, required: true, default: Date.now() },
+  mood: { type: Number, required: true, min: 0, max: 10 },
+  k_good: { type: [String] },
+  k_bad: { type: [String] }
 });
 
 const dayMoodModel = mongoose.model("DayMood", schema);
