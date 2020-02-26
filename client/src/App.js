32fe11a1c948f13103cab2cad78d4541602152c8
Yannick Bourenane <yannick.bourenane@gmail.com>
2020-02-26 17:55:41 +0100

    Client folders/files WIP

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { Route, Switch } from "react-router-dom";
+import "bulma/css/bulma.css";
+
+import Splash from "./views/Splash";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className="section">
+        <div className="container">
+          <Switch>
+            <Route exact path="/" component={Splash} />
+          </Switch>
+        </div>
+      </section>
     </div>
   );
 }
