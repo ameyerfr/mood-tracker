42e539030f8d86b0ac6e19bd59546523bd08de0a
Alex Meyer <alexandre.meyer78@gmail.com>
2020-03-04 11:05:52 +0100

    WIP : contact page

---
+++ client/src/components/contacts/ContactList.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ContactList = () => {
+  return (
+    <>
+    <h2>Your buddies</h2>
+    <div>Contact list !</div>
+    </>
+  )
+};
+
+export default ContactList;
