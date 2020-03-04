74a07247d55a2c9b64efd3789ebafda73d8b597b
Alex Meyer <alexandre.meyer78@gmail.com>
2020-03-04 12:47:46 +0100

    Fix dashboard css

--- client/src/components/DashLink.jsx
+++ client/src/components/DashLink.jsx
@@ -3,9 +3,9 @@ import { Link } from "react-router-dom";
 
 const DashLink = ({title, page, classN}) => {
     return (
-        <div className={`dashlink-wrapper ${classN}`}>
+        <div className={`dashlink-wrapper shadow ${classN}`}>
             <Link to={page}>
-                <div className="dashlink">
+                <div className="dashlink flex-center-column">
                     {title}
                 </div>
             </Link>
