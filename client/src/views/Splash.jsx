1f931709de6932912f007cf55b775bff66f149f0
Kathleen Domingo <kathleensdomingo@gmail.com>
2020-03-04 17:41:56 +0100

    wip splash page

--- client/src/views/Splash.jsx
+++ client/src/views/Splash.jsx
@@ -7,26 +7,33 @@ const Splash = () => {
   return (
     <div className="splash-page">
       <div className="title-wrapper flex-center-column">
-        <img className="splash-img floater" src="/images/logo.png" alt="logo"/>
+        <img className="splash-logo floater" src="/images/logo.png" alt="logo"/>
         <img className="splash-detail" src="/images/++.png" alt="logo"/>
       </div>
-      
-      {/* <img className="splash-img" src="/images/emotions/09.png" alt="mood"/> */}
-      {/* <img className="splash-img" src="/images/pet/egg_evolution.png" alt="egg"/> */}
       <div className="btn-wrapper flex-center-column">
         <Link to="/register"><button className="btn-splash btn-reg">Register</button></Link>
         <Link to="/login"><button className="btn-splash btn-login">Login</button></Link>
       </div>
-      {/* <div>
-        <h3>A mood-tracking application that is also a tamagotchi</h3>
-        <h2>Features:</h2>
-        <ul>
-          <li>Track your mood daily and enter keywords that affected your mood</li>
-          <li>Understand your mood triggers with colorful charts</li>
-          <li>Add friends who can be notified when you have been feeling down.</li>
-          <li>A pet that evolves over time and has </li>
-        </ul>
-      </div> */}
+      <div>
+        {/* <h2>Features:</h2> */}
+        <div className="flex-center-row features-box">
+          <img className="splash-img" src="/images/emotions/01.png"/>
+          <p className="splash-text">Track your mood daily and gain points for your pet</p>
+        </div>
+        <div className="flex-center-row features-box">
+          <p className="splash-text">Understand your mood triggers with cute charts</p>
+          <img className="splash-img" src="/images/emotions/08.png"/>
+        </div>
+        <div className="flex-center-row features-box">
+          <img className="splash-img" src="/images/emotions/04.png"/>
+          <p className="splash-text">Add friends who can be notified when you have been feeling down</p>
+        </div>
+        <div className="flex-center-row features-box">
+          <p className="splash-text">Nourish your virtual pet and watch it evolve over time</p>
+          <img className="splash-img" src="/images/emotions/10.png"/>
+        </div>
+
+      </div>
       {/* <img className="splash-features" src="/images/features.png" alt="features"/> */}
     </div>
   );
