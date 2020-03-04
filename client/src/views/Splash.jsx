e638ba2626756021f4b8c7f541010c8431670c68
Kathleen Domingo <kathleensdomingo@gmail.com>
2020-03-04 16:59:59 +0100

    wip splaash page

--- client/src/views/Splash.jsx
+++ client/src/views/Splash.jsx
@@ -6,17 +6,28 @@ import "../styles/css library/wickedcss.min.css"
 const Splash = () => {
   return (
     <div className="splash-page">
-      <img className="splash-img floater" src="/images/logo.png" alt="logo"/>
+      <div className="title-wrapper flex-center-column">
+        <img className="splash-img floater" src="/images/logo.png" alt="logo"/>
+        <img className="splash-detail" src="/images/++.png" alt="logo"/>
+      </div>
+      
       {/* <img className="splash-img" src="/images/emotions/09.png" alt="mood"/> */}
       {/* <img className="splash-img" src="/images/pet/egg_evolution.png" alt="egg"/> */}
-      <Link to="/register"><button className="btn-splash btn-reg">Register</button></Link>
-      <Link to="/login"><button className="btn-splash btn-login">Login</button></Link>
-      <div>
-        <p>Track your mood and start feeling better !</p>
-        <h2>Features:</h2>
-
-
+      <div className="btn-wrapper flex-center-column">
+        <Link to="/register"><button className="btn-splash btn-reg">Register</button></Link>
+        <Link to="/login"><button className="btn-splash btn-login">Login</button></Link>
       </div>
+      {/* <div>
+        <h3>A mood-tracking application that is also a tamagotchi</h3>
+        <h2>Features:</h2>
+        <ul>
+          <li>Track your mood daily and enter keywords that affected your mood</li>
+          <li>Understand your mood triggers with colorful charts</li>
+          <li>Add friends who can be notified when you have been feeling down.</li>
+          <li>A pet that evolves over time and has </li>
+        </ul>
+      </div> */}
+      {/* <img className="splash-features" src="/images/features.png" alt="features"/> */}
     </div>
   );
 };
