7d40d4bf54e26a5dccc148ed3d3c354cc011e9d0
Kathleen Domingo <kathleensdomingo@gmail.com>
2020-03-04 10:15:00 +0100

    wip back button

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -7,18 +7,18 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 
 import "../styles/navbar.css";
-
+import {useHistory} from "react-router-dom";
 const Navbar = () => {
-
+  let history = useHistory()
   return (
     <nav
       // id=""
       // onClick={}
       className="navbar"
     >
-      <NavLink className="navlinks" to="/dashboard">
+      <button className="navlinks" onClick={() => history.goBack()}>
         <FontAwesomeIcon icon={faArrowAltCircleLeft} />
-      </NavLink>
+      </button>
       <NavLink className="navlinks" to="/dashboard">
         <img className="img-logo" src="/images/emotions/05.png" alt="logo"/>
       </NavLink>
